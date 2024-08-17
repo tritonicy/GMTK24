18a0d76f55ecfb41ec56362c4c0ed38094c4814b
Burak Onur <burakonurbey@gmail.com>
2024-08-17 18:22:47 +0300

    dash deneme

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
@@ -7,8 +8,11 @@ public class PlayerMovement : MonoBehaviour
 {
     private float xMovement;
     private float yMovement;
-    [SerializeField] float moveSpeed;
+    [SerializeField] public float runSpeed;
+    [SerializeField] public float dashSpeed;
+    [HideInInspector] public float moveSpeed;
     [SerializeField] Transform orientation;
+    [SerializeField] Transform playerCam;
     [HideInInspector] public Rigidbody rb;
     [HideInInspector] public bool isGrounded;
     [SerializeField] LayerMask groundLayer;
@@ -18,39 +22,83 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public float jumpForce;
     [SerializeField] public float jumpCooldown;
     [SerializeField] public float airDrag;
-    private float firstSpeed;
+    [SerializeField] public float fallMultiplier = 2.5f;
+    [SerializeField] public float lowJumpMultiplier = 1.5f;
+    [SerializeField] public float dashForce;
+    [SerializeField] public float upwardDashSpeed;
+    [HideInInspector] public bool isDashing = false;
+    [SerializeField] public float dashDuration;
+    [SerializeField] public float moveLockTimer;
+    private float moveLockTimeCounter;
+    [HideInInspector] public bool isMoveLocked;
+    private float desiredMoveSpeed;
+    private float lastDesiredMoveSpeed;
+    private bool isMoveSpeedChanged;
+    private bool keepMomentum;
+    [SerializeField] float speedChangeFactor;
     int count = 0;
 
+    //ziplama updatede calisiyor ilerde fixedupdateye almak gerekebilir.
     private void Start()
     {
+        desiredMoveSpeed = runSpeed;
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         playerStartingHalfHeight = this.transform.localScale.y;
     }
     private void Update()
-    {
-        // velocity biraz olmasi gerekenin ustune cikiyor sorun olursa limit koy.
+    {        
         GatherInput();
         CheckGrounded();
 
-        if(isGrounded) rb.drag = groundDrag;
-        else rb.drag =  0f;
+        if (isGrounded && !isDashing) rb.drag = groundDrag;
+        else rb.drag = 0f;
 
-        firstSpeed = new Vector3(rb.velocity.x, 0f, rb.velocity.z).magnitude;
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing) {
+            isMoveLocked = true;
+            isDashing = true;
+            Dash();
+        }
+
+        if(isDashing) {
+            moveLockTimeCounter += Time.deltaTime;
+            if(moveLockTimeCounter >= moveLockTimer) {
+                moveLockTimeCounter = 0f;
+                isMoveLocked = false;
+                desiredMoveSpeed = runSpeed;
+            }
+        }
+
+        isMoveSpeedChanged = lastDesiredMoveSpeed != desiredMoveSpeed;
+        if (isMoveSpeedChanged)
+        {
+            if (keepMomentum)
+            {
+                StopAllCoroutines();
+                StartCoroutine(SmoothlyLerpMoveSpeed(speedChangeFactor));
+            }
+            else{
+                StopAllCoroutines();
+                moveSpeed = desiredMoveSpeed;
+            }
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
     }
     private void FixedUpdate()
     {
         MovePlayer();
         SpeedControl();
-        Debug.Log(new Vector3(rb.velocity.x,0f,rb.velocity.z).magnitude);
-
+        JumpFallControl();
+        // Debug.Log(new Vector3(rb.velocity.x, 0f, rb.velocity.z).magnitude);
     }
     private void GatherInput()
     {
         xMovement = Input.GetAxisRaw("Horizontal");
         yMovement = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && readyToJump) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && readyToJump)
+        {
             readyToJump = false;
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
@@ -59,14 +107,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
+        if(isMoveLocked) return;
         Vector3 moveVector = orientation.forward * yMovement + orientation.right * xMovement;
-        if(isGrounded) {
+        if (isGrounded)
+        {
             rb.AddForce(moveVector.normalized * moveSpeed * Time.deltaTime * 2f, ForceMode.Force);
         }
-        else{
-            rb.AddForce(moveVector.normalized * moveSpeed * Time.deltaTime * 2f * airDrag , ForceMode.Force);
+        else
+        {
+            rb.AddForce(moveVector.normalized * moveSpeed * Time.deltaTime * 2f * airDrag, ForceMode.Force);
         }
-
     }
 
     private void CheckGrounded()
@@ -76,6 +126,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void SpeedControl()
     {
+        Debug.Log(moveSpeed);
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         if (flatVel.magnitude > moveSpeed * Time.deltaTime / 10)
@@ -85,15 +136,79 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Jump() {
+    private void Jump()
+    {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        rb.AddForce(this.transform.up * jumpForce , ForceMode.Impulse);
+        rb.AddForce(this.transform.up * jumpForce, ForceMode.Impulse);
         Debug.Log(++count);
     }
-
-    private void ResetJump() {
+    private void JumpFallControl()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    private void ResetJump()
+    {
         readyToJump = true;
     }
+    Vector3 forceToApply;
+    private void Dash() {
+        keepMomentum = true;
+        Vector3 lookDir = CalculateDirection(orientation);
+        desiredMoveSpeed = dashSpeed;
 
+        forceToApply = lookDir * dashForce + playerCam.up * upwardDashSpeed;
+        
+        Invoke(nameof(DelayedForceApply), 0.0025f);
+        Invoke(nameof(ResetDash), dashDuration);
+    }
+
+    private Vector3 CalculateDirection(Transform playerCam)
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 returnForce;
+
+        if (verticalInput == 0 && horizontalInput == 0)
+        {
+            returnForce = playerCam.forward;
+        }
+        else
+        {
+            returnForce = playerCam.forward * verticalInput + playerCam.right * horizontalInput;
+        }
+
+        return returnForce.normalized;
+    }
+
+    private void ResetDash() {
+        isDashing = false;
+    }
+    private void DelayedForceApply() {
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+
+    private IEnumerator SmoothlyLerpMoveSpeed(float speedChangeFactor){ 
+        float time = 0f;
+        float difference = MathF.Abs(desiredMoveSpeed - moveSpeed);
+        float startVal = moveSpeed;
+        // Debug.Log(difference);
+
+        while(time < difference) {
+            // Debug.Log(moveSpeed);
+            moveSpeed = Mathf.Lerp(startVal, desiredMoveSpeed , time / difference);
+            time += Time.deltaTime * speedChangeFactor;
+            yield return null;
+        }
+        moveSpeed = desiredMoveSpeed;
+        keepMomentum = false;
+    }
 }
